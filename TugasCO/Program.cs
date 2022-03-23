6a9435922149d0fd88f7bde8b69bea0cbb853ef9
klukman <lukman.arisandi@students.amikom.ac.id>
2022-03-23 21:51:22 +0700

    Add project files.

---
+++ TugasCO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek dari class mahasiswa
+            Taxi taxi = new Taxi();
+
+            // set nilai properties objek saya dan kamu
+            taxi.DriverName = "Lukman";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //memanggil method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ TugasCO/Taxi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas
+{
+    public class Taxi
+    {
+        //properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        //methot
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Drive Name: {0}", DriverName);
+            Console.WriteLine("On Duty: {0}", OnDuty);
+            Console.WriteLine("Number of Passenger: {0}\n", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
